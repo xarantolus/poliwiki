85b974bf2d6f22695e7629728a21154a58d4b8bc
xarantolus <xarantolus@protonmail.com>
2021-03-14 22:40:25 +0100

    Log in to twitter

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module x
 
 go 1.16
+
+require (
+	github.com/dghubble/go-twitter v0.0.0-20201011215211-4b180d0cc78d
+	github.com/dghubble/oauth1 v0.7.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
--- main.go
+++ main.go
@@ -1,16 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"x/config"
 	"x/wikidata"
 	"x/wikipedia"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+)
+
+var (
+	flagConfigFile = flag.String("cfg", "config.yaml", "Config file path")
 )
 
 func main() {
+	flag.Parse()
+
+	cfg, err := config.Parse(*flagConfigFile)
+	if err != nil {
+		panic("parsing configuration file: " + err.Error())
+	}
+
+	log.Println("Fetching politicians...")
+
 	polis, err := wikidata.Politicians()
+	if err != nil {
+		panic("fetching politicians: " + err.Error())
+	}
+
+	var client *twitter.Client
+	{
+		config := oauth1.NewConfig(cfg.Twitter.APIKey, cfg.Twitter.APISecretKey)
+		token := oauth1.NewToken(cfg.Twitter.AccessToken, cfg.Twitter.AccessTokenSecret)
+		httpClient := config.Client(oauth1.NoContext, token)
+
+		client = twitter.NewClient(httpClient)
+
+		selfUser, _, err := client.Accounts.VerifyCredentials(&twitter.AccountVerifyParams{})
+		if err != nil {
+			log.Fatalln("cannot log in with given credentials: " + err.Error())
+		}
+
+		log.Printf("[Twitter]: Logged in @%s\n", selfUser.ScreenName)
+	}
+
+	t, _, err := client.Statuses.Update("This is a test tweet", nil)
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println(t.IDStr)
+
+	log.Println("Start streaming edits...")
 
 	events := wikipedia.StreamEdits(polis.Contains)
 
