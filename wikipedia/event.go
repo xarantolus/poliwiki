6b9a3d264afc27da534583361c7791f2d4971aec
xarantolus <xarantolus@protonmail.com>
2021-03-15 09:11:03 +0100

    Event: generate diff URL

--- wikipedia/event.go
+++ wikipedia/event.go
@@ -1,7 +1,17 @@
 package wikipedia
 
+import (
+	"net/url"
+	"path"
+	"strconv"
+)
+
 type Event struct {
 	Type string `json:"type"`
+
+	Meta struct {
+		URL string `json:"url"`
+	} `json:"meta"`
 
 	// Article title
 	Title string `json:"title"`
@@ -22,6 +32,26 @@ type Event struct {
 
 	// Wiki name, e.g. "dewiki" or "enwiki"
 	Wiki string `json:"wiki"`
+}
+
+func (e *Event) DiffURL() (us string, ok bool) {
+	pageSlug := path.Base(e.Meta.URL)
+	if pageSlug == "" {
+		return
+	}
+
+	var u = url.URL{
+		Scheme: "https",
+		Host:   "de.wikipedia.org",
+		Path:   "/w/index.php",
+	}
+
+	u.Query().Set("title", pageSlug)
+	u.Query().Set("diff", strconv.Itoa(e.Revision.New))
+	u.Query().Set("oldid", strconv.Itoa(e.Revision.Old))
+	u.Query().Set("diffonly", "yes")
+
+	return u.String(), true
 }
 
 type Revision struct {
